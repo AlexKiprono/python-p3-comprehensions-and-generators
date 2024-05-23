ded49ae7d720d50d746d477aba4c8b7206fab62f
AlexKiprono <lexiekiprono@gmail.com>
2024-05-24 00:22:26 +0300

    comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_no = [ i for i in num_list if i % 2 == 0 ]
+    return even_no
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
